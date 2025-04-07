5b30f347b2ee826274c1e8b16274e922c0e0be88
Deepanshusharwan <deepanshusharwan35@gmail.com>
2025-04-07 14:26:42 +0530

    7 april update

---
+++ config/Code - OSS/User/History/-1393f228/1Ho2.py
@@ -0,0 +1,10 @@
+
+a=input("What is the answer to the great question of life, the universe and everything?" ) 
+if a==42: 
+    print("yes") 
+ 
+elif a== "forty two" or a == "forty-two": 
+    print("yes") 
+ 
+else: 
+    print("no")
